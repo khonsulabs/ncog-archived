e262b9958808a72204c3f5b7e60731a4e1962910
Jonathan Johnson <jon@khonsulabs.com>
2020-06-21 08:41:49 -0700

    Adding modal component.

--- khonsuweb/src/lib.rs
+++ khonsuweb/src/lib.rs
@@ -3,6 +3,7 @@ pub mod flash;
 pub mod forms;
 pub mod localization;
 pub mod markdown;
+pub mod modal;
 pub mod static_page;
 pub mod title;
 pub mod validations;
@@ -15,4 +16,16 @@ pub fn wasm_utc_now() -> DateTime<Utc> {
     let nanoes = (timestamp - secs) * 1_000_000_000f64;
     let naivetime = NaiveDateTime::from_timestamp(secs as i64, nanoes as u32);
     DateTime::from_utc(naivetime, Utc)
+}
+
+pub mod prelude {
+    pub use crate::{
+        flash::{self, Flash},
+        forms::prelude::*,
+        modal::Modal,
+        title::Title,
+        validations::prelude::*,
+    };
+    pub use chrono::{naive::NaiveDateTime, DateTime, Utc};
+    pub use std::rc::Rc;
 }
