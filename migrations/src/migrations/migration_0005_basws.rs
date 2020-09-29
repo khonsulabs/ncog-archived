0c51bd3dd10af814b62a427851d9e5ffee5fabac
Jonathan Johnson <jon@khonsulabs.com>
2020-09-29 13:27:52 -0700

    Removing debug migration before merge.

--- migrations/src/migrations/migration_0005_basws.rs
+++ migrations/src/migrations/migration_0005_basws.rs
@@ -5,5 +5,4 @@ pub fn migration() -> Migration {
         // Purposely ignoring not having a down to restore the table.
         .with_up("ALTER TABLE installations ADD COLUMN private_key BYTEA")
         .with_down("ALTER TABLE installations DROP COLUMN IF EXISTS private_key")
-        .debug()
 }
